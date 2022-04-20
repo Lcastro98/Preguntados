472826bb2cde09e24ec42e26f4fee2c1dd7a5391
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-20 17:17:21 -0500

    The array with level 1 questions has been added

---
+++ js/questions.js
@@ -0,0 +1,28 @@
+/**
+ * Level 1 questions
+ */
+const quiz1 = [{
+    question:"¿Cuál de los siguientes sería atraído por un imán?",
+    options:["Metal", "Madera", "Plástico", "Papel"],
+    answer: 0
+},
+{
+    question:"¿Qué nombre tiene tradicionalmente la fiesta que se hace a una mujer que espera un bebé?",
+    options:["Baby drizzle", "Baby downpour", "Baby deluge", "Baby shower"],
+    answer: 3
+},
+{
+    question:"¿En dónde es más probable que escuches la orden \"Limpieza en el pasillo 5\"?",
+    options:["En un banco", "En un supermercado", "En un jardín infantil", "En una montaña rusa"],
+    answer: 1
+},
+{
+    question:"Una canción de cuna se canta para ayudar a los bebés a...",
+    options:["Despertar", "Comer", "Dormir", "Llorar"],
+    answer: 2
+},
+{
+    question:"¿Cuál de estos es un instrumento musical?",
+    options:["Silla", "Corbata", "Bolso", "Guitarra"],
+    answer: 3
+}]
