a79fafa6ab0f7ec15c26b6116c4543a005f86c3c
Adryan <adryanynfante@gmail.com>
2022-04-20 17:01:13 -0500

    js para evaluar preguntar y niveles de quiz

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//CONSTANTES OBTENIDAS DEL HTML
+const numeroPregunta = document.querySelector(".numero-pregunta");
+const textoPregunta = document.querySelector(".texto-pregunta");
+const opcContenedor = document.querySelector(".contenedor-opc");
+const answerIndicadorContenedor = document.querySelector(".indicador-respuesta");
+const inicioBox = document.querySelector(".inicio");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".resultados");
+const cronometro1 = document.querySelector(".cronometro");
+const color_fondo = document.getElementById('color_fondo')
+const topResultados = document.querySelector(".top");
+
+//CONSTANTES PARA LOS NIVELES DE LAS PREGUNTAS
+const nivel1 = quiz;
+const nivel2 = quiz1;
+const nivel3 = quiz2;
+const nivel4 = quiz3;
+const nivel5 = quiz4;
